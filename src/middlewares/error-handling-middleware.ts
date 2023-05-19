a6d61425fc40d34b8e2173355b7376305954f4f0
guilhiz <guilhermevilelasobral@gmail.com>
2023-05-19 13:46:43 -0300

    feat: include booking in query to findRoomsByHotelId

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -61,7 +61,7 @@ export function handleApplicationErrors(
       message: err.message,
     });
   }
-
+  console.log(err);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
     error: 'InternalServerError',
     message: 'Internal Server Error',
