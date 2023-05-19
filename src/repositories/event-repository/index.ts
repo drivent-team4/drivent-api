c07ef4d1d5dbd7c16a089db692f30ae56b2f14e9
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-05-19 10:35:49 -0300

    Feat: add redis on event

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,20 @@
 import { prisma } from '@/config';
+import { redis } from '@/config/redis';
+
+const cacheKey = 'event';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cachedEvent = await redis.get(cacheKey);
+
+  if (cachedEvent) {
+    const event = JSON.parse(cachedEvent) as Event;
+    return event;
+  }
+  const event = await prisma.event.findFirst();
+
+  redis.set(cacheKey, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
