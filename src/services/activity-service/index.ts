e8878db105977bb8916805f7791f36c3ba7eac2f
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-05-25 20:46:11 -0300

    feat: logic for delete an activity by an user

--- src/services/activity-service/index.ts
+++ src/services/activity-service/index.ts
@@ -1,4 +1,4 @@
-import { conflictError, notFoundError } from '@/errors';
+import { cannotDeleteInscriptionError, conflictError, notFoundError } from '@/errors';
 import { forBiddenError } from '@/errors/forbidden-error';
 import activitiesRepository from '@/repositories/activities-repository';
 
@@ -19,7 +19,17 @@ async function deleteInscription(userId: number, inscriptionId: number) {
   const inscription = await activitiesRepository.findInscription(inscriptionId);
   if (!inscription) throw notFoundError;
 
-  if (!(inscription.userId === userId)) throw forBiddenError;
+  if (!(inscription.userId === userId)) throw forBiddenError();
+
+  const activity = await activitiesRepository.getActivityById(inscription.activityId);
+  const dateNow = new Date();
+  const dateActivity = new Date(`${activity.startAt}`);
+
+  const DateDiffInHours = (dateActivity.getTime() - dateNow.getTime()) / (1000 * 60 * 60);
+  if (DateDiffInHours < 24)
+    throw cannotDeleteInscriptionError(
+      'You cannot cancel your registration in the last 24 hours before the start of the event.',
+    );
 
   return await activitiesRepository.deleteInscription(inscriptionId);
 }
